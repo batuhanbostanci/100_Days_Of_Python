a4d9cdfe97e236d076e8453947f15435b3dc6134
Tacettin Batuhan Bostanci <batuhanbostanci2@hotmail.com>
2021-09-07 16:00:57 +0300

    formatting ceaser-cipher

--- ceaser-cipher.py
+++ ceaser-cipher.py
@@ -1,39 +1,39 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
 
 def ceaser():
-  def encrypt(text, shift):
-    tempString =""
-    for i in text:
-      tempIndex = alphabet.index(i) + shift 
-      tempString += tempString.join(alphabet[tempIndex])
-    print(f"The encode text is {tempString}")
-    
-  def decode(text, shift):
-    tempString =""
-    for i in text:
-      tempIndex = alphabet.index(i) - shift 
-      tempString += tempString.join(alphabet[tempIndex])
-    print(f"The encode text is {tempString}")
+    def encrypt(text, shift):
+        temp_string = ""
+        for i in text:
+            temp_index = alphabet.index(i) + shift
+            temp_string += temp_string.join(alphabet[temp_index])
+        print(f"The encode text is {temp_string}")
 
-  if direction=="encode":
-    encrypt(text,shift)
-  else:
-    decode(text, shift)
+    def decode(text, shift):
+        temp_string = ""
+        for i in text:
+            temp_index = alphabet.index(i) - shift
+            temp_string += temp_string.join(alphabet[temp_index])
+        print(f"The encode text is {temp_string}")
+
+    if direction == "encode":
+        encrypt(text, shift)
+    else:
+        decode(text, shift)
+
 
 while True:
-  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-  text = input("Type your message:\n").lower()
-  shift = int(input("Type the shift number:\n"))
-  if(shift > 26):
-      newShift =shift % 26
-      shift = newShift
-  if not text.isalpha():
-    print("Don't use charcter or number")
-  else:
-    
-    ceaser()
-    bool =input("Do you want to keep going?\n")
-    if not bool:
-      break;
-
-  
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if shift > 26:
+        newShift = shift % 26
+        shift = newShift
+    if not text.isalpha():
+        print("Don't use character or number")
+    else:
+        ceaser()
+        booli = input("Do you want to keep going?\n")
+        if not booli:
+            break
