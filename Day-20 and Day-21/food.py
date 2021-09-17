605dfd3e991da2d797f049d403d9be8580138ec6
Tacettin Batuhan Bostanci <batuhanbostanci2@hotmail.com>
2021-09-17 13:49:55 +0300

    editing snake game with read write method

--- Day-20 and Day-21/food.py
+++ Day-20 and Day-21/food.py
@@ -3,11 +3,12 @@ import random
 
 
 class Food(Turtle):
+
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=0.3, stretch_wid=0.3)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
         self.refresh()
