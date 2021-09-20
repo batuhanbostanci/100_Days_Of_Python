1a7cade5197fda9fc3adb8bfdae318a0419f1968
Tacettin Batuhan Bostanci <batuhanbostanci2@hotmail.com>
2021-09-20 12:51:27 +0300

    adding list compherision in for loop

--- Day-25/Us-State-Game/main.py
+++ Day-25/Us-State-Game/main.py
@@ -16,10 +16,11 @@ while len(guessed_states) < 50:
     user_answer = screen.textinput(title=f"{len(guessed_states)}/50 States",
                                    prompt="What is the another state name ").title()
     if user_answer == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
+        # missing_states = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
 
         states_to_learn_csv = pd.DataFrame(missing_states)
         states_to_learn_csv.to_csv("States that need to reviewed ")
