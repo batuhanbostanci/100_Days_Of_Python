e509e322803059a5a74a0ffe5e80cc922b11dea1
Tacettin Batuhan Bostanci <batuhanbostanci2@hotmail.com>
2021-09-29 12:08:30 +0300

    adding try catch block in Day 26 Nato Alphabet

--- Day-26/NATO-alphabet-start/main.py
+++ Day-26/NATO-alphabet-start/main.py
@@ -4,8 +4,17 @@ nato_alphabet_data_frame = pd.read_csv("nato_phonetic_alphabet.csv")
 
 nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet_data_frame.iterrows()}
 
-user_input = input("Write your word\n").upper()
 
-result_list = [nato_alphabet_dict[letter] for letter in user_input]
+def nato_alphabet_generator():
+    try:
+        user_input = input("Write your word\n").upper()
+        result_list = [nato_alphabet_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Please write only letters!!")
+        nato_alphabet_generator()
+    else:
+        print(result_list)
 
-print(result_list)
+
+nato_alphabet_generator()
+
