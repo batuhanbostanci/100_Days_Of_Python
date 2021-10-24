a652fbb58e4fedf56913ab39753e5d949d00cfcb
Tacettin Batuhan Bostanci <batuhanbostanci2@hotmail.com>
2021-10-24 12:51:48 +0300

    final capstone project for Day 39 and Day 40

--- Day-39/README.md
+++ Day-39/README.md
@@ -1,2 +1,8 @@
 ## Capstone Project 
 ### In this capstone project I do cheaper flight finder. In Part-1 deals with only 1 flight but In Part 2 there can be than 1 flight 
+
+* API that I used 
+    * Google Sheet Data Management 
+    * Kiwi Partners Flight Search API (Free Signup, Requires Credit Card Details)
+    * Tequila Flight Search API Documentation 
+    * Twilio SMS API
--- Day-39/data_manager.py
+++ Day-39/data_manager.py
@@ -1,3 +1,27 @@
+import requests
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/**********/flightDealsCopy/prices"
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.destination_data ={}
+
+    def get_destination_data(self):
+        sheet_endpoint = "https://api.sheety.co/***********/flightDealsCopy/prices"
+        response = requests.get(url=sheet_endpoint)
+        response.raise_for_status()
+        sheet_data = response.json()["prices"]
+
+        return sheet_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data ={
+                "price":{
+                    "iataCode": city["iataCode"]
+                }
+            }
+            respone = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(respone.text)
